19d3d167b9406fd11d09d7d0c275713e7d38b295
unknown <marwan.nasser565@gamil.com>
2022-11-21 18:32:07 +0200

    last update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// scroll to top btn 
+const upBtn = document.getElementById("up-btn");
+window.addEventListener("scroll", () => window.scrollY >= 300 ? upBtn.classList.add("show") : upBtn.classList.remove("show"));
+upBtn.addEventListener("click", () => window.scrollTo({ top: 0, left: 0, behavior: "smooth" }))
+
 // Initialize Swiper Slider
 // Set Landing Page Slider
 const swiperElement = document.querySelector(".swiper")
